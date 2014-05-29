7aeed637ba18ef6aa785155feed88f9534719e7e
Takeshi Nagae <takeshi.nagae@gmail.com>
2014-05-29 17:04:20 +0900

    サンプルプログラムを追加

---
+++ cpp/sample1-1.cpp
@@ -0,0 +1,8 @@
+// sample1-1.cpp: 文字列の出力(1)
+#include <iostream>		// 標準入出力ライブラリを読み込む
+using namespace std;		// std という名前空間を使うことを宣言
+
+int main (void) {				 // main 関数の始まり
+  cout << "私の名前は 川内 情太郎 です" << endl; // 標準出力ストリーム(cout)に文字列を出力
+  return 0;					 // 無事に終わったことを表すために 0 を返す
+}						 // main 関数の終わり
